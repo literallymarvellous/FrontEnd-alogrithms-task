47322c52206ee67dc43ac26254f8a1785551f872
literallymarvellous <ahiaraikechukwu@gmail.com>
2021-05-16 23:52:08 +0100

    fixed error

--- convertFahrToCelsius.js
+++ convertFahrToCelsius.js
@@ -1,10 +1,12 @@
 
 function convertFahrToCelsius(n) {
-    if (n instanceof Array) {
+    if (n.length === 0) {
+        return `"${n}" is not a valid number but a/an ${(Array.isArray(n)) ? 'array' : typeof(n)}`;
+    } else if (n instanceof Array) {
         return `${n} is not a valid number but a/an array`;
     } else if (n instanceof Object) {
         return `${JSON.stringify(n)} is not a valid number but an object`;
-    } else if (!Number.isInteger(n)) return `"${n}" is not a valid number but a/an ${typeof(n)}`;
+    } else if (!Number.isInteger(Number(n))) return `"${n}" is not a valid number but a/an ${typeof(n)}`;
     let num = Number(n);
     return ((num - 32) / 1.8).toFixed(4);
 }
